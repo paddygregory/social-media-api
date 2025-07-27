67eba0314aec1b605095e3ba1b2c401ee6b9265c
pgregory <paddygregory62@gmail.com>
2025-07-27 10:22:27 +1000

    modularised celery worker

--- app/worker.py
+++ app/worker.py
@@ -7,28 +7,7 @@ from datetime import datetime, timezone
 import os
 from dotenv import load_dotenv
 import multiprocessing as mp
-
-from celery import Celery
-import os
-
-broker_url = os.getenv("REDIS_BROKER_URL")
-backend_url = os.getenv("REDIS_BACKEND_URL", broker_url)  # fallback to same
-
-# Inject Redis TLS options explicitly
-broker_use_ssl = {
-    "ssl_cert_reqs": "none"  # Use "required" or "optional" for stricter configs
-}
-backend_use_ssl = {
-    "ssl_cert_reqs": "none"
-}
-
-celery_app = Celery(
-    "worker",
-    broker=broker_url,
-    backend=backend_url,
-    broker_use_ssl=broker_use_ssl if broker_url.startswith("rediss://") else None,
-    backend_use_ssl=backend_use_ssl if backend_url.startswith("rediss://") else None
-)
+from app.routes.celery_routes import celery_app
 
 
 @celery_app.task(bind=True)
