9e18d2d3f1c79a5234bf68cd52c367331bdd9b88
pgregory <paddygregory62@gmail.com>
2025-07-27 11:36:10 +1000

    fastapi app

--- app/main.py
+++ app/main.py
@@ -1,20 +1,15 @@
 import os, sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from contextlib import asynccontextmanager
 from sqlmodel import SQLModel
-from .models import Post
 from fastapi import FastAPI
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.routes.health import health_router
 from app.routes.template import template_router
 from app.routes.generate import generate_router
 from app.routes.payments import payments_router
-from .database import engine, create_db_and_tables 
-import uvicorn
-
 from app.routes.auth2 import auth_router
+from app.database import create_db_and_tables
 
 
 @asynccontextmanager
@@ -22,13 +17,28 @@ async def lifespan(app: FastAPI):
     create_db_and_tables()
     yield
 
+
 app = FastAPI(lifespan=lifespan)
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Routers
 app.include_router(health_router)
 app.include_router(template_router)
 app.include_router(generate_router)
 app.include_router(payments_router)
 app.include_router(auth_router)
+
+
 if __name__ == "__main__":
+    import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+
 
