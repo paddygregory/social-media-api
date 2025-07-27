7c2c5bd26d09bf2514f192e460bca868f05fa327
pgregory <paddygregory62@gmail.com>
2025-07-27 10:39:07 +1000

    added ssl args

--- app/celery_app.py
+++ app/celery_app.py
@@ -1,16 +1,23 @@
 
-import os
+# app/celery_app.py
+import os, ssl
 from celery import Celery
 
-broker_url   = os.getenv("REDIS_BROKER_URL")
-backend_url  = os.getenv("REDIS_BACKEND_URL", broker_url)   
+broker_url  = os.getenv("REDIS_BROKER_URL")                      
+backend_url = os.getenv("REDIS_BACKEND_URL", broker_url)        
 
-celery_app = Celery("worker", broker=broker_url, backend=backend_url)
+
+ssl_opts = {"ssl_cert_reqs": ssl.CERT_NONE}      
+
+celery_kwargs = dict(
+    broker   = broker_url,
+    backend  = backend_url,
+)
 
 
 if broker_url.startswith("rediss://"):
-    celery_app.conf.broker_use_ssl = {"ssl_cert_reqs": None}  
-
-
+    celery_kwargs["broker_use_ssl"] = ssl_opts
 if backend_url.startswith("rediss://"):
-    celery_app.conf.redis_backend_use_ssl = {"ssl_cert_reqs": None}
+    celery_kwargs["redis_backend_use_ssl"] = ssl_opts   
+
+celery_app = Celery("worker", **celery_kwargs)
